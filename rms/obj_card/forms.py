91f840cd31b80642fbe9c47b8c734a361eff4a0b
shina74 <103118554+shina74@users.noreply.github.com>
2022-12-10 19:46:10 +0500

    obj_add
    Ссылка - http://127.0.0.1:8000/obj_add

--- rms/obj_card/forms.py
+++ rms/obj_card/forms.py
@@ -3,15 +3,21 @@ from django import forms
 from .models import Object, Picture
 
 
-class ObjForm(forms.ModelForm):
+class ObjForm(forms.Form):
 
-    class Meta:
-        model = Object
-        fields = ('name', 'description',)
+    name = forms.CharField(label=u'Oбъект')
+    dis = forms.CharField(label=u'Описание')
+    photos = forms.ImageField(label=u'Фотографии', widget=forms.FileInput(attrs={'multiple': 'multiple'}))
 
-
-class PicForm(forms.ModelForm):
-
-    class Meta:
-        model = Picture
-        fields = ('image',)
+# class ObjForm(forms.ModelForm):
+#
+#     class Meta:
+#         model = Object
+#         fields = ('name', 'description',)
+#
+#
+# class PicForm(forms.ModelForm):
+#
+#     class Meta:
+#         model = Picture
+#         fields = ('image',)
--- rms/obj_card/urls.py
+++ rms/obj_card/urls.py
@@ -2,14 +2,15 @@ from django.urls import path
 from rest_framework.urlpatterns import format_suffix_patterns
 from . import views
 
-urlpatterns = [
-    path('users/', views.UserList.as_view()),
-    path('users/<int:id>/', views.UserDetail.as_view()),
-    path('obj/', views.ObjList.as_view()),
-    path('obj/<int:id>/', views.ObjDetail.as_view()),
-    path('pic/', views.PicList.as_view()),
-    path('obj_add', views.obj_add, name='obj_add'),
-    path('obj_card/ok', views.ok),
-]
 
+urlpatterns = [
+    #path('users/', views.UserList.as_view()),
+    #path('users/<int:id>/', views.UserDetail.as_view()),
+    #path('obj/', views.ObjList.as_view()),
+    #path('obj/<int:id>/', views.ObjDetail.as_view()),
+    #path('pic/', views.PicList.as_view()),
+    #path('obj/<int:pk>/', views.pic_add, name='pic_add'),
+    path('obj_add', views.obj_add, name='obj_add'),
+    path('obj/<int:pk>/', views.obj_detail, name='obj_detail'),
+]
 urlpatterns = format_suffix_patterns(urlpatterns)
